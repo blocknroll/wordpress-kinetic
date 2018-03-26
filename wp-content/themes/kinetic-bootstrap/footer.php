22f80023d9ada0533efa42ee995911e8cf24ae4b
Fred Block <info@fredblock.com>
2018-03-26 10:27:52 -0500

    Update footer.php with new image links

--- wp-content/themes/kinetic-bootstrap/footer.php
+++ wp-content/themes/kinetic-bootstrap/footer.php
@@ -4,7 +4,12 @@
 
         <p>&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?></p>
 
-        <a href="index.html"><img src="wp-content/themes/kinetic-bootstrap/img/kinetic-shield-17-full-web.svg"></a>
+        <a href= <?php echo esc_url( home_url( '/' ) ); ?> >
+          <img src="
+            <?php echo get_template_directory_uri();
+            ?>/img/kinetic-shield-17-full-web.svg" alt="kinetic logo"
+          />
+        </a>
 
         <address>
           <p><a href="tel:+17202022202">720-202-2202</a></p>
@@ -15,7 +20,8 @@
 
         <div class="socialIcons">
           <p>
-            <a href="https://www.facebook.com/rethinkrestoration" target="_blank">
+            <a href="https://www.facebook.com/rethinkrestoration"
+              target="_blank">
               <span class="pe-so-facebook pe-2x"></span>
             </a>
             <a href="https://www.linkedin.com/company/kinetic-restoration"
@@ -27,14 +33,20 @@
         <div id="bbb">
           <a href="https://www.bbb.org/denver/business-reviews/construction-and-remodeling-services/kinetic-restoration-in-englewood-co-90169775#sealclick"
             target="_blank">
-            <img alt="Click for the BBB Business Review of this Construction &amp;
-            Remodeling Services in Englewood CO" src="wp-content/themes/kinetic-bootstrap/img/bbb-rev.png">
+            <img src="
+              <?php echo get_template_directory_uri();?>/img/bbb-rev.png"
+              alt="Click for the BBB Business Review of this Construction &amp;
+              Remodeling Services in Englewood CO"
+            />
           </a>
         </div>
 
         <div id="iicrc">
           <a href="img/IICRC_KINETIC_RESTORATION.pdf" target="_blank">
-            <img src="wp-content/themes/kinetic-bootstrap/img/IICRC-Certified-Firm-white.png">
+            <img src="
+              <?php echo get_template_directory_uri();
+              ?>/img/IICRC-Certified-Firm-white.png"
+            />
           </a>
         </div>
 
