3a9401a71eea65d31a0ec9cd3a74d1f9ceac9344
Fred Block <info@fredblock.com>
2018-03-29 13:09:37 -0500

    Add page-construction.php. Edit front-page.php and kinetic.css

--- wp-content/themes/kinetic-bootstrap/front-page.php
+++ wp-content/themes/kinetic-bootstrap/front-page.php
@@ -1,183 +1,176 @@
 <?php get_header(); ?>
 
 
-  <!-- INTRO ===================================================== -->
-  <div class="intro watermark_responsive img-responsive">
-
-    <p><span class="dropCap">F</span>our industry experts committed
-    themselves to a better restoration experience and <strong>KINETIC
-    RESTORATION</strong> was born. The vision is clear. Communicate
-    effectively, plan meticulously and deliver flawlessly. The result? A
-    customer experience beyond compare, every customer, every time.</p>
-
-  </div>
+<!-- INTRO ===================================================== -->
+<div class="intro watermark_responsive img-responsive">
+  <p><span class="dropCap">F</span>our industry experts committed
+  themselves to a better restoration experience and <strong>KINETIC
+  RESTORATION</strong> was born. The vision is clear. Communicate
+  effectively, plan meticulously and deliver flawlessly. The result? A
+  customer experience beyond compare, every customer, every time.</p>
+</div>
 
 
+<!-- CAROUSEL ===================================================== -->
+<div id="indexCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
 
-  <!-- CAROUSEL ===================================================== -->
-  <div id="indexCarousel" class="carousel slide carousel-fade" data-ride="carousel">
-    <div class="carousel-inner">
-
-      <div class="item active">
-        <img src="
-          <?php echo get_template_directory_uri();?>/img/home/Pic2-crop-sm.jpg"
-          class="img-responsive center-block" alt="First slide"
-        />
-        <div class="container">
-          <div class="carousel-caption">
-            <h1><a href="tel:+17202022202">720-202-2202</a></h1>
-            <h3 class="emergency">
-              Please call now for an immediate emergency response.
-            </h3>
-            <p>
-              <a class="btn  btn-default"
-                 href="mailto:email@rethinkrestoration.com"
-                 role="button">EMAIL US
-              </a>
-            </p>
-          </div>
+    <div class="item active">
+      <img src="
+        <?php echo get_template_directory_uri();?>/img/home/Pic2-crop-sm.jpg"
+        class="img-responsive center-block" alt="First slide"
+      />
+      <div class="container">
+        <div class="carousel-caption">
+          <h1><a href="tel:+17202022202">720-202-2202</a></h1>
+          <h3 class="emergency">
+            Please call now for an immediate emergency response.
+          </h3>
+          <p>
+            <a class="btn  btn-default"
+               href="mailto:email@rethinkrestoration.com"
+               role="button">EMAIL US
+            </a>
+          </p>
         </div>
       </div>
-
-      <div class="item">
-        <img src="
-          <?php echo get_template_directory_uri();?>/img/projects/01/AFTR-KITCHEN.jpg"
-          class="img-responsive center-block" alt="Second slide"
-        />
-        <div class="container">
-          <div class="carousel-caption">
-            <h1><a href="tel:+17202022202">720-202-2202</a></h1>
-            <h3 class="emergency">
-              Please call now for an immediate emergency response.
-            </h3>
-            <p>
-              <a class="btn  btn-default"
-                 href="mailto:email@rethinkrestoration.com"
-                 role="button">EMAIL US
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="
-          <?php echo get_template_directory_uri();?>/img/home/Pic6-crop-sm.jpg"
-          class="img-responsive center-block" alt="Third slide"
-        />
-        <div class="container">
-          <div class="carousel-caption">
-            <h1><a href="tel:+17202022202">720-202-2202</a></h1>
-            <h3 class="emergency">
-              Please call now for an immediate emergency response.
-            </h3>
-            <p>
-              <a class="btn  btn-default"
-                 href="mailto:email@rethinkrestoration.com"
-                 role="button">EMAIL US
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="
-          <?php echo get_template_directory_uri();?>/img/home/Pic9-crop-sm.jpg"
-          class="img-responsive center-block" alt="Fourth slide"
-        />
-        <div class="container">
-          <div class="carousel-caption">
-            <h1><a href="tel:+17202022202">720-202-2202</a></h1>
-            <h3 class="emergency">
-              Please call now for an immediate emergency response.
-            </h3>
-            <p>
-              <a class="btn  btn-default"
-                 href="mailto:email@rethinkrestoration.com"
-                 role="button">EMAIL US
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="
-          <?php echo get_template_directory_uri();?>/img/home/Pic10-crop-sm.jpg"
-          class="img-responsive center-block" alt="Fifth slide"
-        />
-        <div class="container">
-          <div class="carousel-caption">
-            <h1><a href="tel:+17202022202">720-202-2202</a></h1>
-            <h3 class="emergency">
-              Please call now for an immediate emergency response.
-            </h3>
-            <p>
-              <a class="btn  btn-default"
-                 href="mailto:email@rethinkrestoration.com"
-                 role="button">EMAIL US
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="
-          <?php echo get_template_directory_uri();?>/img/roofing.jpg"
-          class="img-responsive center-block" alt="Sixth slide"
-        />
-        <div class="container">
-          <div class="carousel-caption">
-            <h1><a href="tel:+17202022202">720-202-2202</a></h1>
-            <h3 class="emergency">
-              Please call now for an immediate emergency response.
-            </h3>
-            <p>
-              <a class="btn  btn-default"
-                 href="mailto:email@rethinkrestoration.com"
-                 role="button">EMAIL US
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="
-          <?php echo get_template_directory_uri();?>/img/construction.jpg"
-          class="img-responsive center-block" alt="Seventh slide"
-        />
-        <div class="container">
-          <div class="carousel-caption">
-            <h1><a href="tel:+17202022202">720-202-2202</a></h1>
-            <h3 class="emergency">
-              Please call now for an immediate emergency response.
-            </h3>
-            <p>
-              <a class="btn  btn-default"
-                 href="mailto:email@rethinkrestoration.com"
-                 role="button">EMAIL US
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
-
     </div>
+
+    <div class="item">
+      <img src="
+        <?php echo get_template_directory_uri();?>/img/projects/01/AFTR-KITCHEN.jpg"
+        class="img-responsive center-block" alt="Second slide"
+      />
+      <div class="container">
+        <div class="carousel-caption">
+          <h1><a href="tel:+17202022202">720-202-2202</a></h1>
+          <h3 class="emergency">
+            Please call now for an immediate emergency response.
+          </h3>
+          <p>
+            <a class="btn  btn-default"
+               href="mailto:email@rethinkrestoration.com"
+               role="button">EMAIL US
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="
+        <?php echo get_template_directory_uri();?>/img/home/Pic6-crop-sm.jpg"
+        class="img-responsive center-block" alt="Third slide"
+      />
+      <div class="container">
+        <div class="carousel-caption">
+          <h1><a href="tel:+17202022202">720-202-2202</a></h1>
+          <h3 class="emergency">
+            Please call now for an immediate emergency response.
+          </h3>
+          <p>
+            <a class="btn  btn-default"
+               href="mailto:email@rethinkrestoration.com"
+               role="button">EMAIL US
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="
+        <?php echo get_template_directory_uri();?>/img/home/Pic9-crop-sm.jpg"
+        class="img-responsive center-block" alt="Fourth slide"
+      />
+      <div class="container">
+        <div class="carousel-caption">
+          <h1><a href="tel:+17202022202">720-202-2202</a></h1>
+          <h3 class="emergency">
+            Please call now for an immediate emergency response.
+          </h3>
+          <p>
+            <a class="btn  btn-default"
+               href="mailto:email@rethinkrestoration.com"
+               role="button">EMAIL US
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="
+        <?php echo get_template_directory_uri();?>/img/home/Pic10-crop-sm.jpg"
+        class="img-responsive center-block" alt="Fifth slide"
+      />
+      <div class="container">
+        <div class="carousel-caption">
+          <h1><a href="tel:+17202022202">720-202-2202</a></h1>
+          <h3 class="emergency">
+            Please call now for an immediate emergency response.
+          </h3>
+          <p>
+            <a class="btn  btn-default"
+               href="mailto:email@rethinkrestoration.com"
+               role="button">EMAIL US
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="
+        <?php echo get_template_directory_uri();?>/img/roofing.jpg"
+        class="img-responsive center-block" alt="Sixth slide"
+      />
+      <div class="container">
+        <div class="carousel-caption">
+          <h1><a href="tel:+17202022202">720-202-2202</a></h1>
+          <h3 class="emergency">
+            Please call now for an immediate emergency response.
+          </h3>
+          <p>
+            <a class="btn  btn-default"
+               href="mailto:email@rethinkrestoration.com"
+               role="button">EMAIL US
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="
+        <?php echo get_template_directory_uri();?>/img/construction.jpg"
+        class="img-responsive center-block" alt="Seventh slide"
+      />
+      <div class="container">
+        <div class="carousel-caption">
+          <h1><a href="tel:+17202022202">720-202-2202</a></h1>
+          <h3 class="emergency">
+            Please call now for an immediate emergency response.
+          </h3>
+          <p>
+            <a class="btn  btn-default"
+               href="mailto:email@rethinkrestoration.com"
+               role="button">EMAIL US
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+
   </div>
-  <!-- END CAROUSEL ===================================================== -->
+</div>
+<!-- END CAROUSEL ===================================================== -->
 
 
-
-  <!-- SLOGAN ===================================================== -->
-  <div class="watermark_responsive img-responsive">
-
-    <h1>It’s time to Rethink Restoration.</h1>
-
-  </div>
-
+<!-- SLOGAN ===================================================== -->
+<div class="watermark_responsive img-responsive">
+  <h1>It’s time to Rethink Restoration.</h1>
+</div>
 
 
 <?php get_footer(); ?>
